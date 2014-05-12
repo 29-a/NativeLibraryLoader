d20c37a52e68927ee5d5f3ef7a5926220b2e5f23
Jan Marcel Janik <janmarcel2@web.de>
2014-05-12 22:53:58 +0200

    [2014.05.12_22:53] Initial upload.

---
+++ NativeLibraryLoader/src/de/janik/Main.java
@@ -0,0 +1,32 @@
+package de.janik;
+
+// <- Import ->
+// <- Static_Import ->
+
+/**
+ * @author Jan.Marcel.Janik [©2014]
+ *
+ */
+public final class Main
+{
+	// <- Public ->
+	// <- Protected ->
+	// <- Private->
+	// <- Static ->
+
+	// <- Constructor ->
+	private Main()
+	{
+		throw new IllegalStateException("Do not instanciate !!!");
+	}
+
+	// <- Abstract ->
+	// <- Object ->
+	// <- Getter & Setter ->
+
+	// <- Static ->
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+	}
+}
