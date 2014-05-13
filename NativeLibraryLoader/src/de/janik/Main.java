44d9ff2c14730c8b8c85ed9ec54e6157464038c1
Jan Marcel Janik <janmarcel2@web.de>
2014-05-13 22:05:36 +0200

    [2014.02.13_22:04] Added a test-class, to show how to use the provided classes.

--- NativeLibraryLoader/src/de/janik/Main.java
+++ NativeLibraryLoader/src/de/janik/Main.java
@@ -1,7 +1,12 @@
 package de.janik;
 
 // <- Import ->
+import de.janik.util.jni.NativeLibrary;
+
 // <- Static_Import ->
+import static java.lang.System.out;
+import static de.janik.util.OS.LINUX;
+import static de.janik.util.OS.WINDOWS;
 
 /**
  * @author Jan.Marcel.Janik [©2014]
@@ -9,10 +14,27 @@ package de.janik;
  */
 public final class Main
 {
-	// <- Public ->
-	// <- Protected ->
-	// <- Private->
 	// <- Static ->
+	static
+	{
+		/*
+		 * A Simple C/C++ library, that calls an Assembler-Function that writes the number 8 into the register 'eax'.
+		 * Source-Code is included in the 'lib'-directories.
+		 */
+		NativeLibrary lib = new NativeLibrary("GetInt");
+
+		/*
+		 * Optional, if no dependencies are specified, every file in the directory
+		 * '/lib/"Library-Name"/"Operating-System"/"Architecture"'
+		 */
+		// Windows version compiled with 'Microsoft Visual-Studio 2013 Express (Desktop)' & MASM
+		lib.addDependencie(WINDOWS, "GetInt.dll");
+
+		// Linux version compiled with 'GCC, the GNU Compiler Collection' & NASM
+		lib.addDependencie(LINUX, "libGetInt.so");
+
+		lib.load();
+	}
 
 	// <- Constructor ->
 	private Main()
@@ -20,13 +42,11 @@ public final class Main
 		throw new IllegalStateException("Do not instanciate !!!");
 	}
 
-	// <- Abstract ->
-	// <- Object ->
-	// <- Getter & Setter ->
-
 	// <- Static ->
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		out.println(GetInt());
 	}
+
+	public static native int GetInt();
 }
